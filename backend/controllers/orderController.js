b36f817534e5cf7f05150cc487397b6483399e66
rahulrp17 <rahulrp4021@gmail.com>
2025-07-05 19:22:46 +0530

    Update orderController.js

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -10,7 +10,7 @@ const stripe=new Stripe(process.env.STRIPE_SECRET_KEY);
 
 dotenv.config();
 const placeOrder=async(req,res)=>{
-  const frontend_url="http://localhost:5173";
+  const frontend_url="https://food-delivery-frontend-ib75.onrender.com/";
 
     try {
         const newOrder = new orderModel({
