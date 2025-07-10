ba2435a08803a0771dcada072b28b7ffd5ce2fae
rahulrp17 <rahulrp4021@gmail.com>
2025-07-10 11:57:42 +0530

    Update App.jsx

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -10,7 +10,7 @@ import 'react-toastify/dist/ReactToastify.css'
 
 const App = () => {
    
-   const url = "http://localhost:4000";
+   const url = "https://food-del-backend-393c.onrender.com";
    
   return (
     <div>
