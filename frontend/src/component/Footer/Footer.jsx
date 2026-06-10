edaf1e9f50d36adedbe3b45321c27c8a77237401
rahulrp17 <rahulrp4021@gmail.com>
2026-06-10 16:52:56 +0530

    Update Footer.jsx

--- frontend/src/component/Footer/Footer.jsx
+++ frontend/src/component/Footer/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
     <div className="footer" id="footer">
       <div className="footer-content">
         <div className="footer-content-left">
-          <img src={assets.logo} alt="" />
+          <img src={assets.RP17} alt="" />
           <p>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa
             ducimus reprehenderit incidunt natus voluptatibus nostrum iusto
